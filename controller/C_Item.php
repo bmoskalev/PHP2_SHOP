4b13c2822d4559cd51af5f7f3d54200f647a2398
bmoskalev <cODE2TEACH!>
2019-03-07 00:04:38 +0500

    Add item page

--- controller/C_Item.php
+++ controller/C_Item.php
@@ -18,24 +18,28 @@ class C_Item extends C_Model
         $this->id = (int) $id;
     }
     protected function updateItem(){
-        $this->db::update("UPDATE products SET view_count = view_count + 1 WHERE id = $this->id");
-        $this->product = $this->db::getRow("SELECT * FROM products WHERE id = $this->id");
+        $this->db::update("UPDATE goods SET views = views + 1 WHERE id = $this->id");
+        $this->product = $this->db::getRow("SELECT * FROM goods WHERE id = $this->id");
     }
     public function action_index(){
         self::updateItem();
-        $img = $this->db::getRow("SELECT * FROM images WHERE img_id = {$this->product['img_id']}");
-        $src = $img['main_dir'] . "/" . $img['main_name'];
-        $count = $this->product['view_count'];
-        $product_name = $this->product['product_name'];
-        $description = $this->product['description'];
-        $price = $this->product['price'];
+//        $img = $this->db::getRow("SELECT * FROM images WHERE img_id = {$this->product['img_id']}");
+//        $src = $img['main_dir'] . "/" . $img['main_name'];
+//        $count = $this->product['views'];
+//        $product_name = $this->product['product_name'];
+//        $description = $this->product['description'];
+//        $price = $this->product['price'];
 
-        $this->render('product_page.tmpl', [
-            'src' => $src,
-            'price' => $price,
-            'count' => $count,
-            'name' => $product_name,
-            'description' => $description]);
+        $this->render('item.tmpl',
+//            [
+//            'src' => $src,
+//            'price' => $price,
+//            'count' => $count,
+//            'name' => $product_name,
+//            'description' => $description
+//        ]
+            $this->product
+        );
     }
 }
 
